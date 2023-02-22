c3f2b9d083f2a1f162dd459c570ba8535a64b2b6
nguyentu111 <tunguyen118108@gmail.com>
2023-02-22 23:00:39 +0700

    added description page, finished main page ✔✔✨✨

--- app/src/main/java/com/example/myapplication/DescriptionPage.java
+++ app/src/main/java/com/example/myapplication/DescriptionPage.java
@@ -1,18 +1,139 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
 
 public class DescriptionPage extends AppCompatActivity {
     LinearLayout topBanner;
+    ImageView imgCoverDes;
+    TextView textDes;
+    TextView textNameDes;
+    LinearLayout altTitles;
+    Context context ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description_page);
-        topBanner = findViewById(R.id.topbanner);
+        imgCoverDes = findViewById(R.id.imgCoverDes);
+        textDes= findViewById(R.id.textDes);
+        textNameDes= findViewById(R.id.textNameDes);
+        context = getApplicationContext();
+        loadInfo();
+    }
+    private void loadInfo(){
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) return ;
+        String id = extras.getString("mangaId");
+        String url= "https://api.mangadex.org/manga/"+id;
+        RequestQueue queue = Volley.newRequestQueue(this);
 
+        TextView textView = findViewById(R.id.textview);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String des ="";
+                        String title = "";
+                        try {
+
+
+                            JSONObject data=   response.getJSONObject("data");
+                            String mangaId = data.getString("id");
+                            JSONObject description=   data.getJSONObject("attributes").getJSONObject("description");
+                            JSONObject titleObject=   data.getJSONObject("attributes").getJSONObject("title");
+                            String imgId="";
+
+                            JSONArray relationships = data.getJSONArray("relationships");
+                            for(int j =0;j<relationships.length();j++){
+                                if(relationships.getJSONObject(j).getString("type").equals("cover_art")){
+                                    imgId=relationships.getJSONObject(j).getString("id");
+                                }
+                            }
+
+                            loadImg(mangaId,imgId,imgCoverDes);
+                            for (final Iterator<String> iter1 = description.keys(); iter1.hasNext();) {
+                                String key1 = iter1.next();
+                                des= description.getString(key1);
+                            }
+                            Log.v("daasd",titleObject.toString());
+                            for (final Iterator<String> iter2 = titleObject.keys(); iter2.hasNext();) {
+                                String key2 = iter2.next();
+                                title= titleObject.getString(key2);
+                            }
+                            textDes.setText("\t\t\t"+des);
+                            textNameDes.setText(title);
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+        queue.add(jsonObjectRequest);
+
+    }
+    private void loadImg(String mangaId, String imgId, ImageView img){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.mangadex.org/cover/" +imgId;
+        TextView textView = findViewById(R.id.textview);
+        int imgWidth =120;
+        int imgHeight =(int) imgWidth *3 / 2;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject data=   response.getJSONObject("data");
+                            String imgUrl = "https://uploads.mangadex.org/covers/"+mangaId+"/"+data.getJSONObject("attributes").getString("fileName");
+                            Picasso.get()
+                                    .load(imgUrl)
+                                    .resize(imgWidth, imgHeight)
+                                    .centerCrop()
+                                    .into(img);
+
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+        queue.add(jsonObjectRequest);
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,14 +7,17 @@ import androidx.gridlayout.widget.GridLayout;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -23,17 +26,23 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.github.ybq.android.spinkit.SpinKitView;
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.CubeGrid;
+import com.github.ybq.android.spinkit.style.FoldingCube;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Iterator;
+
 public class MainActivity extends AppCompatActivity {
     Button btn ;
     GridLayout gridView;
     Context context ;
-    int gridLayoutGap = 8;
+    int gridLayoutGap = 16;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,14 +60,15 @@ public class MainActivity extends AppCompatActivity {
 //                }
 //            });
         //////////////
-        gridView= findViewById(R.id.gridview);
+
+                gridView= findViewById(R.id.gridview);
         context = getApplicationContext();
         loadMangas();
 
 
     }
     private void loadMangas(){
-        String url="https://api.mangadex.org/manga";
+        String url="https://api.mangadex.org/manga?limit=50";
         RequestQueue queue = Volley.newRequestQueue(this);
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
@@ -66,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onResponse(JSONObject response) {
-
-
+                        int imgWidth = (gridView.getMeasuredWidth()-(gridLayoutGap*6))/3;
+                        int imgHeight =(int) imgWidth *3 / 2;
                         try {
                             JSONArray datas=   response.getJSONArray("data");
                             for(int i =0;i<datas.length();i++){
@@ -82,6 +92,13 @@ public class MainActivity extends AppCompatActivity {
                                 ImageView imgView = new ImageView(context);
                                 imgView.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                                 CardView cardView = new CardView(context);
+                                ImageView imgViewFallback = new ImageView(context);
+                                imgViewFallback.setImageResource(R.drawable.fallbackimg);
+                                ViewGroup.LayoutParams imgParams = new ViewGroup.LayoutParams(imgWidth,imgHeight);
+                                imgViewFallback.setLayoutParams(imgParams);
+                                cardView.addView(imgViewFallback);
+                                cardView.setMinimumWidth(imgWidth);
+                                cardView.setMinimumHeight(imgHeight);
                                 cardView.setRadius(20);
                                 String mangaId = data.getString("id");
                                 JSONArray relationships = data.getJSONArray("relationships");
@@ -91,15 +108,21 @@ public class MainActivity extends AppCompatActivity {
                                         imgId=relationships.getJSONObject(j).getString("id");
                                     }
                                 }
-                                loadImg(mangaId,imgId,imgView);
+                                loadImg(mangaId,imgId,imgView,cardView);
                                 cardView.addView(imgView);
+
                                 manga.addView(cardView);
                                 ////////// name and chap
                                 LinearLayout info = new LinearLayout(context);
                                 info.setOrientation(LinearLayout.VERTICAL);
                                 TextView mangaName = new TextView(context);
-                                String name = data.getJSONObject("attributes").getJSONObject("title").getString("en");
-                                if(name.length()>32) name=name.substring(0,32)+"...";
+                                String name="" ;
+                                JSONObject titles = data.getJSONObject("attributes").getJSONObject("title");
+                                for (final Iterator<String> iter = titles.keys(); iter.hasNext();) {
+                                    String key = iter.next();
+                                    name= titles.getString(key);
+                                }
+                                if(name.length()>25) name=name.substring(0,25)+"...";
                                 mangaName.setText(name);
                                 mangaName.setTextSize(15);
                                 mangaName.setGravity(Gravity.CENTER_HORIZONTAL);
@@ -116,6 +139,14 @@ public class MainActivity extends AppCompatActivity {
                                 info.addView(mangaName);
                                 info.addView(mangaLastChap);
                                 manga.addView(info);
+                            manga.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Intent i = new Intent(MainActivity.this, DescriptionPage.class);
+                                    i.putExtra("data",data);
+                                    startActivity(i);
+                                }
+                            });
                                 //////////
                                 gridView.addView(manga);
                             }
@@ -137,12 +168,12 @@ public class MainActivity extends AppCompatActivity {
                 });
         queue.add(jsonObjectRequest);
     }
-    private void loadImg(String mangaId,String imgId,ImageView img){
+    private void loadImg(String mangaId,String imgId,ImageView img,CardView cardView){
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://api.mangadex.org/cover/" +imgId;
         TextView textView = findViewById(R.id.textview);
         int imgWidth = (gridView.getMeasuredWidth()-(gridLayoutGap*6))/3;
-        int imgHeight =(int) imgWidth *4 / 3;
+        int imgHeight =(int) imgWidth *3 / 2;
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     @Override
@@ -155,6 +186,7 @@ public class MainActivity extends AppCompatActivity {
                                     .resize(imgWidth, imgHeight)
                                     .centerCrop()
                                     .into(img);
+
                         } catch (JSONException e) {
                             throw new RuntimeException(e);
                         }
@@ -181,10 +213,18 @@ public class MainActivity extends AppCompatActivity {
                             float maxChapter =0;
                             JSONArray datas=   response.getJSONArray("data");
                             for(int i=0;i<datas.length();i++){
-                                float chap = Float.parseFloat(datas.getJSONObject(i).getJSONObject("attributes").getString("chapter"));
-                                if(maxChapter<chap) maxChapter=chap;
+                                String f = datas.getJSONObject(i).getJSONObject("attributes").getString("chapter");
+                                if(!f.equals("null")){
+                                    float chap = Float.parseFloat(f);
+                                    if(maxChapter<chap) maxChapter=chap;
+                                }
+
                             }
-                            textView.setText("Chap "+Float.toString(maxChapter));
+                            if(maxChapter==0.0)  textView.setText("Oneshot");
+                            else {
+                                if((int) maxChapter % 1 ==0)  textView.setText("Chap "+Float.toString((int) maxChapter));
+                                    else textView.setText("Chap "+Float.toString(maxChapter));
+                            };
                         } catch (JSONException e) {
                             throw new RuntimeException(e);
                         }
